45636853654da23fcb4cb525e8f8771ee906ae55
Mahmoud Nagy <m.nagy@nilebot.com>
2019-11-07 11:40:38 +0200

    forcing the new code style

--- parser_config_template.h
+++ parser_config_template.h
@@ -15,25 +15,25 @@
 
 /*INTERNAL DATA STRUCTURE*/
 static struct {
-	uint8_t ucMagicNumber;
-	uint8_t ucI2cAddress;
-	char cName[configNAME_LEN];
-	/*EXTEND ME*/
+    uint8_t ucMagicNumber;
+    uint8_t ucI2cAddress;
+    char cName[configNAME_LEN];
+    /*EXTEND ME*/
 } xParams;
 /*INTERNAL DATA STRUCTURE END*/
 
 /*REQUIRED MACROS*/
-#define configREAD_PARAMS() do{;}while(0)
-#define configUPDATE_PARAMS() do{;}while(0)
-#define configPARAMS_SET_DEFAULT_EXTRA() do{;}while(0);
-#define configINIT_EXTRA() do{;}while(0)
+#define configREAD_PARAMS() do {; } while (0)
+#define configUPDATE_PARAMS() do {; } while (0)
+#define configPARAMS_SET_DEFAULT_EXTRA() do {; } while (0);
+#define configINIT_EXTRA() do {; } while (0)
 #define configEXECUTE_READ()
 #define configEXECUTE_READ_PRINTF_STRING ""
 /*REQUIRED MACROS END*/
 
 /*OPTIONAL MACROS*/
-#define configEXTRA_COMMANDS(cmd,buffer) {\
-	return false;\
+#define configEXTRA_COMMANDS(cmd, buffer) { \
+        return false; \
 }
 /*OPTIONAL MACROS END*/
 
